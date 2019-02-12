8423b79557f5019e793edd1a2695984b83010a37
mGiorgiutti <mathieu.giorgiutti@gfi.fr>
2019-02-12 16:11:49 +0100

    set toggle button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgxSelectComponent } from '../lib/components/ngx-select/ngx-select.component';
 import { NgxSelectChildrenComponent } from '../lib/components/ngx-select-children/ngx-select-children.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
     declarations: [AppComponent, NgxSelectComponent, NgxSelectChildrenComponent],
-    imports: [BrowserModule],
+    imports: [BrowserModule, FontAwesomeModule],
     providers: [],
     bootstrap: [AppComponent]
 })
--- src/lib/components/ngx-select/ngx-select.component.ts
+++ src/lib/components/ngx-select/ngx-select.component.ts
@@ -1,13 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
+import { faCaretRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'ngx-select',
     templateUrl: './ngx-select.component.html',
-    styleUrls: ['./ngx-select.component.scss']
+    styleUrls: ['./ngx-select.component.scss'],
+    // tslint:disable-next-line:use-host-property-decorator
+    host: {
+        '(document:click)': 'onClick($event)'
+    }
 })
 export class NgxSelectComponent implements OnInit {
-    constructor() {}
+    // icon
+    public faCaretRight = faCaretRight;
+    // @Input()
+    // public items: Item[] = [];
+    @Input()
+    public defaultToggleButtonLabel = 'No Items Selected';
+    @Input()
+    public placeholder = 'Search Items';
+    @Input()
+    public toggleBtnClass = 'w-75';
+    @Input()
+    public toggleContentClass = 'w-100';
+
+    public state = 'closed';
+    public toggleButtonLabel = 'default';
+
+    constructor(private _eref: ElementRef) {}
 
     ngOnInit() {}
+
+    animateToggle() {
+        this.state = this.state === 'closed' ? 'open' : 'closed';
+    }
+
+    onClick(event) {
+        if (!this._eref.nativeElement.contains(event.target)) {
+            if (this.state === 'open') {
+                this.animateToggle();
+            }
+        }
+    }
 }
