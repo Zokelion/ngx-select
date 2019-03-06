ae4cc617e28656ca92369fd51e50215f5baacb77
mGiorgiutti <mathieu.giorgiutti@gfi.fr>
2019-03-06 17:46:40 +0100

    enable access viewchild to many property

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,1 +1,4 @@
 export * from './ngx-select.module';
+export * from './components/ngx-select/ngx-select.component';
+export * from './models/item-selected-event.model';
+export * from './models/item.model';
